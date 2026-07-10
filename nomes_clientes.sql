e78c8243470477775379c389a8dc97a2056f5a52
nathalya <arq.nathalya@gmail.com>
2026-07-09 22:02:22 -0300

    Código criado em nomes_clientes.sql

--- nomes_clientes.sql
+++ nomes_clientes.sql
@@ -1,2 +1,5 @@
--- SQLite (Exercicio do Curso SQL da IBM: Objetivo: aprender a escrever pseudocódigo)
--- Pegar todos os primeiros e últimos nomes de todos os clientes da tabela Customer.
+-- Listar todos os primeiros e últimos nomes de todos os clientes da tabela Customer que moram em São Paulo, em ordem alfabética.
+SELECT FirstName, LastName
+FROM Customer
+WHERE City = 'São Paulo'
+ORDER BY FirstName ASC;
