682148366d7d9a8642c52a5b3beaf5c20443e3a0
nathalya <arq.nathalya@gmail.com>
2026-07-11 02:06:02 -0300

    Aumento de complexidade: add JOIN nos códigos.
    Adicionado o comando JOIN nos arquivos:
        - artistas_compositores.sql
        - faturas_datas.sql

--- artistas_compositores.sql
+++ artistas_compositores.sql
@@ -1,19 +1,30 @@
--- Descobrir IDs dos gêneros Clássico e Eletrônico (independentemente de acentos ou idioma).
+/*
+  OBJETIVO: Descobrir IDs dos gêneros Clássico e Eletrônico.
+
+  LÓGICA:
+    - Identificar tabela de origem: 'Genre' (id, gênero).
+    - Filtrar por gênero (clássico ou eletrônico) independentemente de acentos ou idioma.  
+*/
 
 SELECT * FROM Genre
 WHERE Name LIKE '%ssic%' OR Name LIKE '_le%' ;
 
 
 
--- Listar nomes dos compositores, das músicas e preços da tabela Track,
--- cujos gêneros são 24-Clássico ou 15-Eletrônico,
--- cujos preços são menores que 1.99,
--- sem valores nulos para compositores,
--- em ordem crescente de nomes de músicas, primeiro as clássicas, depois as eletrônicas.
+/*
+  OBJETIVO: Listar músicas dos gêneros Clássico e Eletrônico com preço promocional.
+  
+  LÓGICA:
+    - Identificar tabelas de origem: 'Track' (compositor, música, preço) e 'Genre' (gênero musical).
+    - Filtrar faixas por gênero (24-Clássico ou 15-Eletrônico) e preço (< 1.99).
+    - Limpar dados: descartar faixas sem compositor definido (NULL).
+    - Ordenação: priorizar agrupamento por Gênero e, dentro dele, o nome da faixa.
+*/
 
-SELECT Composer, Name, UnitPrice 
+SELECT Genre.Name, Track.Composer, Track.Name, Track.UnitPrice 
 FROM Track
-WHERE (GenreId = 24 OR GenreId = 15)
-  AND UnitPrice < 1.99 
-  AND Composer IS NOT NULL 
-ORDER BY GenreId DESC, Composer ASC;
+JOIN Genre ON Track.GenreId = Genre.GenreId
+WHERE (Track.GenreId = 24 OR Track.GenreId = 15)
+  AND Track.UnitPrice < 1.99 
+  AND Track.Composer IS NOT NULL 
+ORDER BY Track.GenreId DESC, Track.Composer ASC;
--- faturas_datas.sql
+++ faturas_datas.sql
@@ -1,6 +1,14 @@
--- Listar todas as datas de faturas e valores totais menores que 5.00, em ordem decrescente de data.
+/*
+  OBJETIVO: Listar clientes com histórico de faturas de baixo valor (< 5.00). 
 
-SELECT InvoiceDate, Total
-FROM Invoice
-WHERE Total < 5.00
-ORDER BY InvoiceDate DESC;
+  - Identificar tabelas de origem: 'Customer' (nome cliente) e 'Invoice' (data fatura, valor fatura).
+  - Unir tabelas (JOIN) para listar só faturas com nome de cliente cadastrado.
+  - Filtrar por valor da fatura (< 5.00). 
+  - Ordenar: agrupar por nome do cliente (ASC) e data da fatura (DESC). 
+*/
+
+SELECT Customer.FirstName, Customer.LastName, Invoice.InvoiceDate, Invoice.Total
+FROM Customer
+JOIN Invoice ON Customer.CustomerId = Invoice.CustomerId
+WHERE Invoice.Total < 5.00
+ORDER BY Customer.FirstName, Invoice.InvoiceDate DESC;
