a2bdc67d67feb9ebbf14262f53b86b088129efac
nathalya <arq.nathalya@gmail.com>
2026-07-12 21:10:21 -0300

    Adição de comandos como CONCAT e FORMAT.

--- faturas_datas.sql
+++ faturas_datas.sql
@@ -2,16 +2,17 @@
 OBJETIVO: Listar clientes com histórico de faturas de baixo valor (< 5.00). 
 
 LÓGICA:
-- Identificar tabelas de origem: 'Customer' (nome cliente) e 'Invoice' (data fatura, valor fatura).
+- Identificar tabelas de origem: 
+    'Customer' (nome cliente),
+    'Invoice' (data fatura - dd-MM-yyyy, valor fatura).
 - Unir tabelas (JOIN) para listar só faturas com nome de cliente cadastrado.
 - Filtrar por valor da fatura (< 5.00). 
 - Ordenar: agrupar por nome do cliente (ASC) e data da fatura (DESC). 
 */
 
 SELECT
-    Customer.FirstName, 
-    Customer.LastName, 
-    Invoice.InvoiceDate, 
+    CONCAT(Customer.FirstName, ' ', Customer.LastName), 
+    FORMAT(Invoice.InvoiceDate, 'dd-MM-yyyy'), 
     Invoice.Total
 FROM Customer
 JOIN Invoice ON Customer.CustomerId = Invoice.CustomerId
